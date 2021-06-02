471c54a69e90e3c35b2522ee840418217a467f93
L1ncd4w9 <82919695+L1ncd4w9@users.noreply.github.com>
2021-06-02 18:57:51 -0400

    Inital Commit

---
+++ Server.js
@@ -0,0 +1,36 @@
+/* const express = require('express');
+const app = express();
+const port = 3002;
+var data;
+
+app.post('/', (req, res) => {
+  console.log(req.body.data)
+  res.send("hi")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
+*/
+
+const express = require('express')
+const app = express()
+var port = 69;
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body.hello)
+})
+
+app.listen(port, () =>{
+
+
+})
